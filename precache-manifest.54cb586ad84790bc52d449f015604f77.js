b8534b1f7f7ab282b2964236b6dc9310aa7c88ec
Abhishekkumar Ashokkumar <abhishekkumar.ashokkumar@vivy.com>
2022-03-06 18:28:34 +0100

    Updates

---
+++ precache-manifest.54cb586ad84790bc52d449f015604f77.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc720619256af2a3e960e966d38af4ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8f8f60c99c9f254eea3",
+    "url": "./static/css/main.4c1d1e7e.chunk.css"
+  },
+  {
+    "revision": "f34aa0ba608a60bc66e9",
+    "url": "./static/js/2.17e52322.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.17e52322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f8f60c99c9f254eea3",
+    "url": "./static/js/main.2fc3ccfe.chunk.js"
+  },
+  {
+    "revision": "a5935f4634d1dec88798",
+    "url": "./static/js/runtime-main.fcfe2d94.js"
+  },
+  {
+    "revision": "46001e05fa14bbe9dae04792d9b0fad6",
+    "url": "./static/media/chat_e.46001e05.svg"
+  },
+  {
+    "revision": "8378708a13586922c7614134bd0f5ba6",
+    "url": "./static/media/diet.8378708a.svg"
+  },
+  {
+    "revision": "94d0e3974824347ebfb9e897e0d4e5ea",
+    "url": "./static/media/doctor.94d0e397.svg"
+  },
+  {
+    "revision": "c22fcdc0ce2e1eefcd256ee4fe024c44",
+    "url": "./static/media/emoti.c22fcdc0.svg"
+  },
+  {
+    "revision": "063a17a4fc1ced13aeb7f778a704949b",
+    "url": "./static/media/locker.063a17a4.svg"
+  },
+  {
+    "revision": "824e7bc3e2cacc12579ac38b19361d42",
+    "url": "./static/media/logo.824e7bc3.svg"
+  },
+  {
+    "revision": "e1a32aa32fa3fe550bbfb34b6efe476a",
+    "url": "./static/media/music.e1a32aa3.svg"
+  },
+  {
+    "revision": "404bb9d2e1c77ef942fe11df58f73b42",
+    "url": "./static/media/running_r.404bb9d2.svg"
+  }
+]);
